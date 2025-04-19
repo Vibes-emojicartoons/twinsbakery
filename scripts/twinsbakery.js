1bc4c15b1fefa568faa615b6658f987ffd96fb44
Vibes-emojicartoons <koketsomatshe@gmail.com>
2025-04-19 02:29:44 +0200

    I managed to add chat-display and also double checking my html, managed to tell script to note the following.

---
+++ scripts/twinsbakery.js
@@ -0,0 +1,9 @@
+// twinsbakery.js//
+let cart=[];
+let totalCost = 0;
+const cartCountSpan = document.getElementById('cart-count');
+const totalCostSpan = document.getElementById('total-cost');
+const selectedItemsList = document.getElementById('selected-items');
+const fileInput = document.getElementById('file-upload');
+const fileNameDisplay = document.getElementById('file-name');
+const chatDisplay = document.getElementById('chat-display');
