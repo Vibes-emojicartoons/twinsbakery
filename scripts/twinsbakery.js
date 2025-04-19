68ba4dcd327c0a4d076f679c5ccc4d315fa1fbeb
Vibes-emojicartoons <koketsomatshe@gmail.com>
2025-04-19 02:50:14 +0200

    Told script that there is a fucntion to add items on the cart on the follwing sections.

--- scripts/twinsbakery.js
+++ scripts/twinsbakery.js
@@ -6,4 +6,19 @@ const totalCostSpan = document.getElementById('total-cost');
 const selectedItemsList = document.getElementById('selected-items');
 const fileInput = document.getElementById('file-upload');
 const fileNameDisplay = document.getElementById('file-name');
-const chatDisplay = document.getElementById('chat-display');
+const chatDisplay = document.getElementById('cart-display');
+
+// Update the cart count on navigation//
+function updateCartCount() {
+  cartCountSpan.textContent = cart.length;
+}
+
+// Fuction to add seleceted items to cart//
+function addSelectedItemsToCart() {
+    const minicakeSection = document.getElementById('minicake');
+    const mixedGoodiesSection = document.getElementById('mixedgoodies');
+    const celebrationCakeSection = document.getElementById('celebrationcake');
+    const cakeToppingSection = document.getElementById('cake-topping');
+
+    
+}
