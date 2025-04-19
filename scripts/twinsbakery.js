e9201338ebe0c73a2a4e06ca5bfe02d92411aeea
Vibes-emojicartoons <koketsomatshe@gmail.com>
2025-04-19 03:21:00 +0200

    Added a selctor query for each section.

--- scripts/twinsbakery.js
+++ scripts/twinsbakery.js
@@ -15,10 +15,26 @@ function updateCartCount() {
 
 // Fuction to add seleceted items to cart//
 function addSelectedItemsToCart() {
+    // let's work with mini cakes//
     const minicakeSection = document.getElementById('minicake');
+    const minicakeChecked = minicakeSection.querySelector(".minicake").checked;
+
+
+
+
+    // let's work with mixed goodies//
     const mixedGoodiesSection = document.getElementById('mixedgoodies');
+    const mixedGoodiesChecked = mixedGoodiesSection.querySelector(".bottom-mixedgoodies").checked;
+
+
+    // let's work with celebration cakes//
     const celebrationCakeSection = document.getElementById('celebrationcake');
+    const celebrationCakeChecked = celebrationCakeSection.querySelector(".bottom-celebration").checked;
+
+
+    // let's work with cake toppings//
     const cakeToppingSection = document.getElementById('cake-topping');
+    const cakeToppingChecked = cakeToppingSection.querySelector(".topping").checked;
 
     
 }
